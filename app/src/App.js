a0be378216d08694f66e15e960d0d5fe312f34a4
Tim Morrise <timothy.morrise@gmail.com>
2020-08-01 23:34:25 -0600

    messing with react-calendar

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from "react-calendar"
+import 'react-calendar/dist/Calendar.css'
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(new Date())
+
+  const handleChange = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Calendar 
+        onchange={handleChange}
+      />
+      <h1>{inputs.date}</h1>
     </div>
   );
 }
