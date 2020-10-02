c9713ee0ef024afc09af38ae7e111fbd91a09b92
Tim Morrise <timothy.morrise@gmail.com>
2020-10-02 00:03:56 -0600

    added az time

--- app/src/components/TimesContainer.js
+++ app/src/components/TimesContainer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Clock from 'react-live-clock'
+import moment from 'moment'
 
 const TimesContainer = () => {
-    let 
+    let today = new Date()
+    let arizTimeZone = moment(today).isDST() ? "US/Pacific" : "US/Mountain"
     return (
         <div>
             Times container placeholder
+         
             <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
             <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Mountain'} />
             <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Central'} />
